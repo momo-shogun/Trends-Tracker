bf8386cd8a3082b8e7df993245aa452d0d176fb5
Krishna S Negi <123064537+momo-shogun@users.noreply.github.com>
2024-12-01 08:27:34 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,6 @@ export default function App() {
             <Routes>
                 <Route
                     index
-                    path="dashboard"
                     element={
                         <RecoilRoot>
                             <DashboardPage />
