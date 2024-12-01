4e700e406a61820300c71fd505da924953ea74d4
Krishna S Negi <123064537+momo-shogun@users.noreply.github.com>
2024-12-01 08:26:44 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,6 @@ export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route  element={<LandingPage />} />
                 <Route
                     index
                     path="dashboard"
